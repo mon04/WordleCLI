1a64cf35acc166097283d60285221530004de780
Michael <michael.oneill.2020@mumail.ie>
2022-02-10 17:51:08 +0000

    clean code

--- src/Cell.java
+++ src/Cell.java
@@ -5,29 +5,17 @@ public class Cell {
     private final char value;
     private State state;
 
-    /**
-     * Constructor
-     * @param value the letter value of this cell
-     */
     public Cell(char value) {
         this.value = value;
         this.state = null;
     }
 
-    /**
-     * Get the letter coloured with ANSI
-     * @return cell's letter coloured by state with an ANSI escape code
-     */
     public String visualised() {
         if (state == null)
             return " "+value+" ";
         return String.format("%s %c ", state.getAnsiCode(), value);
     }
 
-    /**
-     * Get the cell's letter as a char
-     * @return the cell's letter as a char
-     */
     public char getValue() {
         return value;
     }
--- src/Row.java
+++ src/Row.java
@@ -6,9 +6,6 @@ public class Row {
     private final Cell[] cells;
     private boolean isEvaluated;
 
-    /**
-     * @param cellValues A string of the letter values for each cell
-     */
     public Row(String cellValues) {
         length=cellValues.length();
         cells = new Cell[length];
@@ -16,9 +13,6 @@ public class Row {
             cells[i] = new Cell(cellValues.charAt(i));
     }
 
-    /**
-     * @return a char array of the letters from each cell
-     */
     public char[] toCharArray() {
         char[] out = new char[length];
         for(int i=0; i < length; i++)
@@ -26,9 +20,6 @@ public class Row {
         return out;
     }
 
-    /**
-     * @return each cell's letter concatenated in a string
-     */
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
@@ -47,38 +38,21 @@ public class Row {
         return sb.toString();
     }
 
-    /**
-     * Determines whether the letters in this row form the wordle
-     * @param wordle the solution word to be compared against
-     * @return true/false whether or not this row's word matches the wordle
-     */
     public boolean equals(String wordle) {
         return this.toString().equals(wordle);
     }
 
-    /**
-     *
-     * @return whether or not this row has yet been evaluated
-     */
     public boolean isEvaluated() {
         return isEvaluated();
     }
 
-    /**
-     * Set the state (green, yellow, gray) of each cell in the row.
-     *
-     * First it finds all green cells, then yellows, and then the rest
-     * are set as gray.
-     *
-     * @param wordle the answer against which to evaluate
-     */
     public void evaluateAgainst(String wordle) {
         char[] rowTemp = this.toCharArray();
         char[] wordleTemp = wordle.toCharArray();
         for(int i=0; i < length; i++) { // Determine greens
             Cell cell = cells[i];
             if (rowTemp[i] != '_' && rowTemp[i] == wordleTemp[i]) {
-                rowTemp[i] = '_';
+                //rowTemp[i] = '_';
                 wordleTemp[i] = '_';
                 cell.setState(State.GREEN);
             }
@@ -90,7 +64,7 @@ public class Row {
         }
         for(int i=0; i < length; i++) { // Determine grays
             Cell cell = cells[i];
-            if(cell.getState() != State.GREEN && cell.getState() != State.YELLOW)
+            if(cell.getState() == null)
                 cell.setState(State.GRAY);
         }
         isEvaluated=true;
